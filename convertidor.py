f64d84d2d4658788e9c4e4b7a201191127cb550b
PablisMB <pablismb@gmail.com>
2022-09-23 21:34:34 +0200

    +5 funciones
    5 funciones nuevas!
    - Decimal a octal
    - Octal a decimal
    - Decimal a hexadecimal
    - Hexadecimal a decimal
    - Decimal a ASCII

--- convertidor.py
+++ convertidor.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-# Versión: 1.0
+# Versión: 1.1
 
 import time
 import webbrowser
-import os, sys
 
 print("Hecho por @PablisMB")
 
@@ -11,13 +10,22 @@ def main():
 	print("Elige de que a que pasar el número")
 	print("1. Decimal a binario")
 	print("2. Binario a decimal")
+	print("3. Decimal a octal")
+	print("4. Octal a decimal")
+	print("5. Decimal a hexadecimal")
+	print("6. Hexadecimal a decimal")
+	print("7. Decimal a ASCII")
 	opcion = int(input("Elige una opción: "))
 	
 	def decimalABinario(numero_decimal):
-		if(numero_decimal > 1):
-			decimalABinario(numero_decimal//2)
-
-		print(numero_decimal%2, end=' ')
+		if numero_decimal <= 0:
+			return "0"
+		binario = ""
+		while numero_decimal > 0:
+			residuo = int(numero_decimal % 2)
+			numero_decimal = int(numero_decimal / 2)
+			binario = str(residuo) + binario
+		print(binario)
 
 	def binarioADecimal(numero_binario):
 		numero_decimal = 0 
@@ -27,9 +35,79 @@ def main():
 
 		return numero_decimal
 
+	def decimalAOctal(numero_decimal):
+		octal = ""
+		while numero_decimal > 0:
+			residuo = numero_decimal % 8
+			octal = str(residuo) + octal
+			numero_decimal = int(numero_decimal / 8)
+		return octal
+
+	def octalADecimal(numero_octal):
+			decimal = 0
+			posicion = 0
+			numero_octal = numero_octal[::-1]
+			for digito in numero_octal:
+				valor_entero = int(digito)
+				numero_elevado = int(8 ** posicion)
+				equivalencia = int(numero_elevado * valor_entero)
+				decimal += equivalencia
+				posicion += 1
+
+			return decimal
+
+	def decimalAHexadecimal(numero_decimal):
+		hexadecimal = ""
+		while numero_decimal > 0:
+			residuo = numero_decimal % 16
+			if residuo == 10:
+				residuo = "A"
+			elif residuo == 11:
+				residuo = "B"
+			elif residuo == 12:
+				residuo = "C"
+			elif residuo == 13:
+				residuo = "D"
+			elif residuo == 14:
+				residuo = "E"
+			elif residuo == 15:
+				residuo = "F"
+			hexadecimal = str(residuo) + hexadecimal
+			numero_decimal = int(numero_decimal / 16)
+		return hexadecimal
+
+	def hexadecimalADecimal(numero_hexadecimal):
+		decimal = 0
+		posicion = 0
+		numero_hexadecimal = numero_hexadecimal[::-1]
+		for digito in numero_hexadecimal:
+			if digito == "A":
+				digito = 10
+			elif digito == "B":
+				digito = 11
+			elif digito == "C":
+				digito = 12
+			elif digito == "D":
+				digito = 13
+			elif digito == "E":
+				digito = 14
+			elif digito == "F":
+				digito = 15
+			valor_entero = int(digito)
+			numero_elevado = int(16 ** posicion)
+			equivalencia = int(numero_elevado * valor_entero)
+			decimal += equivalencia
+			posicion += 1
+
+		return decimal
+
+	def decimalAAscii(numero_decimal):
+		ascii = chr(numero_decimal)
+		return ascii
+
 	if opcion == 1:
-		numero_decimal = int(input("Introduce un número decimal: "))
-		decimalABinario(numero_decimal)
+		decimal = int(input("Introduce un número decimal: "))
+		decimalABinario(decimal)
 		main()
 
 	elif opcion == 2:
@@ -37,6 +115,31 @@ def main():
 		print(binarioADecimal(numero_binario))
 		main()
 	
+	elif opcion == 3:
+		decimal= int(input("Introduce un número decimal: "))
+		print(decimalAOctal(decimal))
+		main()
+	
+	elif opcion == 4:
+		numero_octal= input("Introduce un número octal: ")
+		print(octalADecimal(numero_octal))
+		main()
+
+	elif opcion == 5:
+		decimal= int(input("Introduce un número decimal: "))
+		print(decimalAHexadecimal(decimal))
+		main()
+
+	elif opcion == 6:
+		numero_hexadecimal= input("Introduce un número hexadecimal: ")
+		print(hexadecimalADecimal(numero_hexadecimal))
+		main()
+
+	elif opcion == 7:
+		decimal= int(input("Introduce un número decimal: "))
+		print(decimalAAscii(decimal))
+		main()
+
 	elif opcion == 69:
 		print("Número gracioso")
 		main()
